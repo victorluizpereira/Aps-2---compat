f64660d87dec6fb25c9124ac5b58101a0d2d8cea
victorlp17408 <victorluizpereira17408@gmail.com>
2018-10-27 17:35:24 -0300

    first commit

---
+++ Aps visual/Aps visual/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Aps_visual
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btndecifrar = new System.Windows.Forms.Button();
+            this.btncifrar = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.TextBox3 = new System.Windows.Forms.RichTextBox();
+            this.TextBox2 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.btndecifrar);
+            this.panel1.Controls.Add(this.btncifrar);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.TextBox3);
+            this.panel1.Controls.Add(this.TextBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.TextBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(509, 454);
+            this.panel1.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 297);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Texto cifrado para decifrado:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Texto criptografado:";
+            // 
+            // btndecifrar
+            // 
+            this.btndecifrar.Location = new System.Drawing.Point(366, 424);
+            this.btndecifrar.Name = "btndecifrar";
+            this.btndecifrar.Size = new System.Drawing.Size(75, 23);
+            this.btndecifrar.TabIndex = 10;
+            this.btndecifrar.Text = "DECIFRAR";
+            this.btndecifrar.UseVisualStyleBackColor = true;
+            this.btndecifrar.Click += new System.EventHandler(this.btndecifrar_Click);
+            // 
+            // btncifrar
+            // 
+            this.btncifrar.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncifrar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btncifrar.Location = new System.Drawing.Point(366, 173);
+            this.btncifrar.Name = "btncifrar";
+            this.btncifrar.Size = new System.Drawing.Size(75, 23);
+            this.btncifrar.TabIndex = 9;
+            this.btncifrar.Text = "CIFRAR";
+            this.btncifrar.UseVisualStyleBackColor = true;
+            this.btncifrar.Click += new System.EventHandler(this.btncifrar_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(362, 193);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(79, 81);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(380, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Usuário 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(380, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Usuário 1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(339, 293);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(127, 125);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // TextBox3
+            // 
+            this.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBox3.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox3.Location = new System.Drawing.Point(17, 313);
+            this.TextBox3.Name = "TextBox3";
+            this.TextBox3.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.TextBox3.Size = new System.Drawing.Size(279, 105);
+            this.TextBox3.TabIndex = 4;
+            this.TextBox3.Text = "";
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBox2.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox2.Location = new System.Drawing.Point(17, 193);
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.TextBox2.Size = new System.Drawing.Size(279, 101);
+            this.TextBox2.TabIndex = 3;
+            this.TextBox2.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(338, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 125);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.AcceptsTab = true;
+            this.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBox1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox1.Location = new System.Drawing.Point(17, 58);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.TextBox1.Size = new System.Drawing.Size(279, 109);
+            this.TextBox1.TabIndex = 1;
+            this.TextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(302, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mensagem enviadas de maneira segura ao usuario secundário";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 452);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Mensagens";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox TextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.RichTextBox TextBox3;
+        private System.Windows.Forms.RichTextBox TextBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btncifrar;
+        private System.Windows.Forms.Button btndecifrar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Aps visual/Aps visual/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+using Cripto;
+
+namespace Aps_visual
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int x; 
+
+        private void btncifrar_Click(object sender, EventArgs e)
+        {
+            string palavraenv, palavrarece;     // palavraenv para enviar, palavrarece para receber
+             string condição = "cifrar";         // condição para o arquivo Cript.cs para cifrar ou decifrar 
+
+            palavraenv = Convert.ToString(TextBox1.Text);   // palavraenv recebe o texto da textBox1
+        
+            this.x = palavraenv.Length;         // enviando a quandidade de caracteres para o objeto x
+
+            // palavrarece recebe o texto string retornado pelo método script da classe arqc
+
+            palavrarece = arqc.script(palavraenv, condição, this.x);
+
+            // imprime nas textview2 e 3 o texto retornado
+
+            TextBox2.Text = palavrarece;
+            TextBox3.Text = palavrarece;
+        }
+                               
+
+        private void btndecifrar_Click(object sender, EventArgs e)
+        {
+            string palavracomp, palavralimp;  // palavracomp recebe a palavra composta da textview, 
+                                          // palavralimp recebe texto decifrado
+            string condição = "decifrar";     // condição decifrar ao método script
+       
+            palavracomp = Convert.ToString(TextBox2.Text); // palavracomp recebe texto composto
+
+            // palavralimp recebe a string do método script da class arqc que retornará o texto decifrado
+
+            palavralimp = arqc.script(palavracomp, condição, this.x);
+
+            TextBox3.Text = palavralimp; // Imprime a palavralimp na textview 3
+        }
+
+    }
+
+ }
+
